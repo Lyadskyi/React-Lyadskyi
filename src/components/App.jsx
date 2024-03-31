640568d97261c4f2de6eee8931f10e73d6d0a7bd
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-03-31 18:04:00 +0300

    add default property values in Product.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,6 @@
 // src/components/App.jsx
 
-// import { Product } from "./Product";
-
-// export default function App() {
-  // return (
-    // <div>
-      // <h1>Best selling</h1>
-
-      // <Product
-      //   name="Tacos With Lime"
-      //   imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-      //   price={10.99}
-      // />
-      // <Product
-      //   name="Fries and Burger"
-      //   imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-      //   price={14.29}
-      // />
-//     </div>
-//   );
-// }
-
-import { Product } from './Product';
+import Product from './Product';
 
 export default function App() {
   return (
@@ -30,18 +9,12 @@ export default function App() {
 
       <Product
         name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
         price={10.99}
       />
       <Product
         name="Fries and Burger"
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
-      />
-      <Product
-        name="Fruits and Vegetables"
-        imgUrl="https://www.shlomosaspen.com/wp-content/uploads/2023/04/fruits-and-vegetables-most-use-in-restaurants.jpeg"
-        price={15.74}
       />
     </div>
   );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,52 +1,15 @@
 // src/components/Product.jsx
 
-// export const Product = () => {
-//   const price = 999;
-//   const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
-
-//   return (
-//     <>
-//       <h2>Tacos</h2>
-//       <img src={imgUrl} alt="Tacos With Lime" width="640" />
-//       <p>Price: {price} credits</p>
-//     </>
-//   );
-// };
-
-// export default Product;
-
-// export const Product = props => {
-//   return (
-//     <div>
-//       <h2>{props.name}</h2>
-//       <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="480" />
-//       <p>Price: 999 credits</p>
-//     </div>
-//   );
-// };
-
-// export default Product;
-
-// export const Product = props => {
-//   return (
-//     <div>
-//       <h2>{props.name}</h2>
-//       <img src={props.imgUrl} alt={props.name} width="480" />
-//       <p>Price: {props.price} credits</p>
-//     </div>
-//   );
-// };
-
-// export default Product;
-
-export const Product = ({ name, imgUrl, price }) => {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <img src={imgUrl} alt={name} width="480" />
-      <p>Price: {price} credits</p>
-    </div>
-  );
-};
+const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+    <h2>{name}</h2>
+    <img src={imgUrl} alt={name} width="480" />
+    <p>Price: {price} credits</p>
+  </div>
+);
 
 export default Product;
