86584df633f9e9510f72876d26a8e15fdf877863
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-04-07 18:55:44 +0300

    add first form

--- src/components/CustomButton/CustomButton.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/CustomButton.jsx
-// Reading props/Читання props
-
-const CustomButton = ({ message, children }) => {
-  return <button onClick={() => console.log(message)}>{children}</button>;
-};
-export default CustomButton;
