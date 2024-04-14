4c6d82380eb715b3550290ced063b5a1b8bdd046
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-04-15 00:45:11 +0300

    Finished 3rd module 2st lesson

---
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -0,0 +1,84 @@
+import { useId } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { ErrorMessage } from "formik";
+import css from "./FeedbackForm.module.css";
+
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  message: Yup.string()
+    .min(3, "Too short")
+    .max(256, "Too long")
+    .required("Required"),
+  level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
+});
+
+const initialValues = {
+  username: "",
+  email: "",
+  message: "",
+  level: "good",
+};
+
+const FeedbackForm = () => {
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const msgFieldId = useId();
+  const levelFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form className={css.form}>
+        <label htmlFor={nameFieldId}>Username</label>
+        <Field
+          className={css.field}
+          type="text"
+          name="username"
+          id={nameFieldId}
+        />
+        <ErrorMessage className={css.error} name="username" component="span" />
+        <label htmlFor={emailFieldId}>Email</label>
+        <Field
+          className={css.field}
+          type="email"
+          name="email"
+          id={emailFieldId}
+        />
+        <ErrorMessage name="email" component="span" />
+        <label htmlFor={msgFieldId}>Message</label>
+        <Field
+          className={css.textArea}
+          as="textarea"
+          name="message"
+          id={msgFieldId}
+          rows="5"
+        />
+        <ErrorMessage name="message" component="span" />
+        <label htmlFor={levelFieldId}>Service satisfaction level</label>
+        <Field as="select" name="level" id={levelFieldId}>
+          <option value="good">Good</option>
+          <option value="neutral">Neutral</option>
+          <option value="bad">Bad</option>
+        </Field>
+        <ErrorMessage name="level" component="span" />
+        <button className={css.btn} type="submit">
+          Submit
+        </button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FeedbackForm;
