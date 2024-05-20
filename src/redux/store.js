d2eb391d44fee910310643e3831d64f979e32e53
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-05-21 00:06:18 +0300

    Added actions & reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,39 @@
 // src/redux/store.js
 
-import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { createStore } from "redux";
+
+// // Початкове значення стану Redux для кореневого редюсера, якщо не передати параметр preloadedState.
+
+// const initialState = {
+//   tasks: [
+//     { id: 0, text: "Learn HTML and CSS", completed: true },
+//     { id: 1, text: "Get good at JavaScript", completed: true },
+//     { id: 2, text: "Master React", completed: false },
+//     { id: 3, text: "Discover Redux", completed: false },
+//     { id: 4, text: "Build amazing apps", completed: false },
+//   ],
+//   filters: {
+//     status: "all",
+//   },
+// };
+
+// // Поки що використовуємо редюсер який тільки повертає отриманий стан
+
+// const rootReducer = (state = initialState, action) => {
+//   return state;
+// };
+
+// // Створюємо розширення стора, щоб додати інструменти розробника
+
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+// src/redux/store.js
+
 import { createStore } from "redux";
-
-// Початкове значення стану Redux для кореневого редюсера, якщо не передати параметр preloadedState.
-
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: "all",
-  },
-};
-
-// Поки що використовуємо редюсер який тільки повертає отриманий стан
-
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
-
-// Створюємо розширення стора, щоб додати інструменти розробника
+import { devToolsEnhancer } from "@redux-devtools/extension";
+import { rootReducer } from "./reducers";
 
 const enhancer = devToolsEnhancer();
 export const store = createStore(rootReducer, enhancer);
